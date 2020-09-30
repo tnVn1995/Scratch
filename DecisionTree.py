ccd5ce94e8e6fa2f709eb8169b7bf5d3de0c5f15
tung2921 <tnguyen1@iwu.edu>
2020-09-29 23:41:42 -0500

    Check submodule

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,14 +1,52 @@
 """Implementing Id3 Decision Tree Classification Algorithm"""
+from typing import List, Any
+from collections import Counter
+import math
 import pandas as pd
 import numpy as np
-from typing import List
-import math
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn')
+from sklearn import datasets
+
 
 def entropy(probs: List[float]) -> float:
     """Compute the entropy given a list of class probabilities"""
+
     return sum(-p * math.log(p, 2) for p in probs if p > 0)
+
 
 assert entropy([1.0]) == 0, "Wrong implementation!"
 assert entropy([0.5, 0.5]) == 1, "Wrong implementation!"
 
+test = [0]
 
+
+def class_probabilities(labels: List[Any]) -> List[float]:
+    """Calculate class probabilities from data labels"""
+
+    total_obs = len(labels)
+    probs = [p / total_obs for p in Counter(labels).values()]
+    return probs
+
+
+def info_gain(labels: List[Any]) -> float:
+    """Calculate entropy from data"""
+    return entropy(class_probabilities(labels))
+
+
+assert info_gain(['a']) == 0
+assert info_gain([True, False]) == 1
+
+
+def partition_entropy(vars: List[List[Any]]) -> float:
+    """Return the partition entropy"""
+    total_count = sum(len(var) for var in vars)
+
+
+def main():
+    X, y = datasets.load_boston(return_X_y=True)
+    pass
+
+if __name__ == '__main__':
+    main()
--- LR.py
+++ LR.py
@@ -4,23 +4,25 @@ from typing import List
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression as LinearR
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
+
+plt.style.use('seaborn')
 import time
+
 
 class LinearRegression:
     """Perform a linear regression on the data"""
-    def __init__(self, R_squared: float=0.0, rmse: float=0.0) -> None:
+
+    def __init__(self, R_squared: float = 0.0, rmse: float = 0.0) -> None:
         """[Linear Regression model metrics]
 
         Args:
             R_squared (float, optional): [coefficient determination value]. Defaults to 0.0.
             rmse (float, optional): [root mean squared error]. Defaults to 0.0.
-        """       
+        """
         self.R_squared = R_squared
         self.rmse = rmse
-        
 
-    def fit(self, X: List[float], y: List[float], norm: bool=True):
+    def fit(self, X: List[float], y: List[float], norm: bool = True):
         """[Fit a regression line on the data]
 
         Args:
@@ -30,7 +32,7 @@ class LinearRegression:
 
         Returns:
             LR object
-        """        
+        """
         if norm:
             X = (X - np.mean(X)) / np.linalg.norm(X)
         ones = np.ones(X.shape[0])[:, np.newaxis]
@@ -56,33 +58,34 @@ class LinearRegression:
 
         rmse = np.sqrt(np.mean(numerator))
         return rmse
-    
+
 
 def main():
     print('[INFO] Fitting scratched LR on the boston dataset...')
     X, y = datasets.load_boston(return_X_y=True)
-    x = X[:,1].reshape(-1,1)
+    x = X[:, 1].reshape(-1, 1)
     LR = LinearRegression()
     LR.fit(x, y)
     time.sleep(1)
 
-# plt.plot(x,, color = 'k')
+    # plt.plot(x,, color = 'k')
 
     print('[INFO] Fitting sklearn LR on the boston dataset ...')
     lr = LinearR()
-    lr.fit(x,y)
+    lr.fit(x, y)
     preds = lr.predict(x)
     time.sleep(1)
     print('[INFO] Compare model performance ...')
-    time.sleep(1)  
-    fig, (ax1, ax2) = plt.subplots(1,2)  
-    ax1.scatter(x, y, color = 'b')
-    ax1.plot(x,LR.preds, color = 'r')
+    time.sleep(1)
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.scatter(x, y, color='b')
+    ax1.plot(x, LR.preds, color='r')
     ax1.set_title('Scratched LR fitted line')
-    ax2.scatter(x, y, color = 'b')
-    ax2.plot(x,preds, color = 'orange')
+    ax2.scatter(x, y, color='b')
+    ax2.plot(x, preds, color='orange')
     ax2.set_title('sklearn LR fitted line')
     plt.show()
+
 
 # import plotly.graph_objects as go
 # fig = go.Figure()
@@ -91,7 +94,6 @@ def main():
 # fig.add_trace(go.Line(x=x.squeeze(), y=preds, name='Sklearn Fitted Line'))
 # fig.add_trace(go.Line(x=x.squeeze(), y=LR.preds, name='Scratched LR Fitted Line'))
 # fig.update_layout(title='Comparison of Sklearn and Scratched models')
-
 
 
 if __name__ == "__main__":
